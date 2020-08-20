5cc34e3648be57cbaefd9c8fe61c896696b9e448
pedrozc90 <pedrozc90@gmail.com>
2020-08-19 22:19:05 -0300

    update shaman spells

--- spells/shaman.lua
+++ spells/shaman.lua
@@ -19,36 +19,17 @@ SpellList["SHAMAN"] = {
         filter = "HELPFUL",
 
         -- All Specs
-        { spellID = 974   , caster = "player", filter = "BUFF" },   -- Earth Shield
-        { spellID = 108271, caster = "player", filter = "BUFF" },   -- Astral Shift
-        { spellID = 260881, caster = "player", filter = "BUFF" },   -- Spirit Wolf
         
         -- Elemental
-        { spellID = 108281, caster = "player", filter = "BUFF" },   -- Ancestral Guidance
-        { spellID = 114050, caster = "player", filter = "BUFF" },   -- Ascendance
-        { spellID = 191634, caster = "player", filter = "BUFF" },   -- Stormkeeper
-        { spellID = 263806, caster = "player", filter = "BUFF" },   -- Wind Gust
-
-            -- Procs
-            { spellID = 77762 , caster = "player", filter = "BUFF" },   -- Lava Surge
-            { spellID = 118522, caster = "player", filter = "BUFF" },   -- Elemental Blast: Critical Strike
-            { spellID = 173183, caster = "player", filter = "BUFF" },   -- Elemental Blast: Haste
-            { spellID = 173184, caster = "player", filter = "BUFF" },   -- Elemental Blast: Mastery
-            { spellID = 210174, caster = "player", filter = "BUFF" },   -- Icefury
-            { spellID = 260734, caster = "player", filter = "BUFF" },   -- Master of the Elements
-            { spellID = 272737, caster = "player", filter = "BUFF" },   -- Unlimited Power
-            { spellID = 285514, caster = "player", filter = "BUFF" },   -- Surge of Power
+        { spellID = 324  , caster = "player", filter = "BUFF" }, -- Lightning Shield (Rank 1)
+        { spellID = 325  , caster = "player", filter = "BUFF" }, -- Lightning Shield (Rank 2)
+        { spellID = 905  , caster = "player", filter = "BUFF" }, -- Lightning Shield (Rank 3)
+        { spellID = 945  , caster = "player", filter = "BUFF" }, -- Lightning Shield (Rank 4)
+        { spellID = 8134 , caster = "player", filter = "BUFF" }, -- Lightning Shield (Rank 5)
+        { spellID = 10431, caster = "player", filter = "BUFF" }, -- Lightning Shield (Rank 6)
 
         -- Restoration
-        { spellID = 61295 , caster = "player", filter = "BUFF" },   -- Riptide
-        { spellID = 79206 , caster = "player", filter = "BUFF" },   -- Spiritwalker's Grace
-        { spellID = 114052, caster = "player", filter = "BUFF" },   -- Ascendance
-        { spellID = 207400, caster = "player", filter = "BUFF" },   -- Ancestral Vigor
-        { spellID = 288675, caster = "player", filter = "BUFF" },   -- High Tide
         
-            -- Procs
-            { spellID = 53390 , caster = "player", filter = "BUFF" },   -- Tidal Waves
-            { spellID = 216251, caster = "player", filter = "BUFF" },   -- Undulation
     }, -- [1]
     --[[
     {
@@ -93,36 +74,22 @@ SpellList["SHAMAN"] = {
         ----------------------------------------------------------------
         -- Buffs
         ----------------------------------------------------------------
-        { spellID = 974   , caster = "player", filter = "BUFF" },   -- Earth Shield
-        
         -- Restoration
-        { spellID = 61295 , caster = "player", filter = "BUFF" },   -- Riptide
-        { spellID = 207400, caster = "player", filter = "BUFF" },   -- Ancestral Vigor
 
         ----------------------------------------------------------------
         -- Debuffs
         ----------------------------------------------------------------
-        -- Hex
-        { spellID = 51514 , caster = "player", filter = "DEBUFF" },  -- Hex: Frog
-        { spellID = 210873, caster = "player", filter = "DEBUFF" }, -- Hex: Compy
-        { spellID = 211004, caster = "player", filter = "DEBUFF" }, -- Hex: Spider
-        { spellID = 211010, caster = "player", filter = "DEBUFF" }, -- Hex: Snake
-        { spellID = 211015, caster = "player", filter = "DEBUFF" }, -- Hex: Cockroach
-        { spellID = 269352, caster = "player", filter = "DEBUFF" }, -- Hex: Skeletal Hatchling
-        { spellID = 277778, caster = "player", filter = "DEBUFF" }, -- Hex: Zandalari Tendonripper
-        { spellID = 277784, caster = "player", filter = "DEBUFF" }, -- Hex: Wicker Mongrel
 
         -- Elemental
-        { spellID = 51490 , caster = "player", filter = "DEBUFF" }, -- Thunderstorm
-        { spellID = 182387, caster = "player", filter = "DEBUFF" }, -- Earthquake
-        { spellID = 188389, caster = "player", filter = "DEBUFF" }, -- Flame Shock
-        { spellID = 196840, caster = "player", filter = "DEBUFF" }, -- Frost Shock
+        { spellID = 8050 , caster = "player", filter = "DEBUFF" }, -- Flame Shick (Rank 1)
+        { spellID = 8052 , caster = "player", filter = "DEBUFF" }, -- Flame Shick (Rank 2)
+        { spellID = 8053 , caster = "player", filter = "DEBUFF" }, -- Flame Shick (Rank 3)
+        { spellID = 10447, caster = "player", filter = "DEBUFF" }, -- Flame Shick (Rank 4)
+        { spellID = 10448, caster = "player", filter = "DEBUFF" }, -- Flame Shick (Rank 5)
 
         -- Restoration
-        { spellID = 188838, caster = "player", filter = "DEBUFF" }, -- Flame Shock
 
         -- Totems
         { spellID = 3600  , caster = "player", filter = "DEBUFF" },  -- Earthbind
-        { spellID = 118905, caster = "player", filter = "DEBUFF" },  -- Stack Charge
     }, -- [3]
 }
